efb62e07bef2ec8479e42ed8e77b707a28b2583c
grungytinman <31602218+grungytinman@users.noreply.github.com>
2019-10-30 14:14:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+$(document).keydown(function(e){
+    if(e.which === 123){
+       return false;
+    }
+});
+
+
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
