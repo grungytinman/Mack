6fa72661e012ed2752682d226e9984388ceb71cc
grungytinman <31602218+grungytinman@users.noreply.github.com>
2019-10-30 14:05:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2721,16 +2721,4 @@ window.onkeydown = function(e) {
     }
 };
 
-$(document).keydown(function(e){
-    if(e.which === 123){
- 
-       return false;
- 
-    }
- 
-});
 
-$(document).bind("contextmenu",function(e) { 
-	e.preventDefault();
- 
-});
